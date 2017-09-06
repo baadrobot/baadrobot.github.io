e14a3cb7b1ac3c3ad76d6fbc5cdc002112a1ec9d
baadrobot <baadrobot92@gmail.com>
2017-09-06 11:49:01 +0300

    Animated-list added

---
+++ myworks/animated-list/myscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var opacLi = $('#idUl li').css('opacity');
+	$('#idUl li').hover(function(){
+		$('#idUl li').not($(this)).stop().animate({opacity:'0.3', bottom: '0'}, 100);
+		
+	}, function(){
+		$('#idUl li').not($(this)).stop().animate({opacity: opacLi, bottom: '3px'}, 100);
+	});
+}); // конец ready
